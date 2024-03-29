ed72535ff8e968cb8603637badc64751c14b45ba
Ford1508 <phuclc1508@gmail.com>
2024-03-29 16:21:12 +0700

    init test cicd demo app

---
+++ src/Components/User/index.tsx
@@ -0,0 +1,16 @@
+
+
+const Users = () => {
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          <li>name 1</li>
+          <li>name 2</li>
+          <li>name 3</li>
+        </ul>
+      </div>
+    );
+};
+  
+export default Users;
